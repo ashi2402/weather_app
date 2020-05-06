176fdf47f3e5c9c3a3c8deb6da795002593e65a7
ashish240284 <ashish240284@gmail.com>
2020-05-06 18:15:57 +0530

    add about content

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('request');
 
 const forecast = ( longitude, latitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=3965ab9afd27cf23c5a82747da9d2dd5&query=${latitude},${longitude}&units=f`
-
+    // const degree = <span>&#8451;</span>
     request({url, json: true}, (error, {body})=> {
         if(error){
             callback('Unable to connect with weather service', undefined)
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degress Fahrenheit out. There is a '+ body.current.precip + ' % chance of rain today.'  )
+            callback(undefined, 'It is currently ' + body.current.temperature +' Degree Fahrenhiet out. There is a '+ body.current.precip + ' % chance of rain today.'  )
         }
     })
 }
